2ae110cddba7fca347d9e27c4bc1e82d163ed219
Luiz Eduardo do Prado <prado.eduardo.luiz@gmail.com>
2021-10-01 18:53:10 -0300

    feat: update GitHubService with api params

--- data/src/main/java/br/com/prado/eduardo/luiz/githubrepositories/data/source/remote/service/GitHubService.kt
+++ data/src/main/java/br/com/prado/eduardo/luiz/githubrepositories/data/source/remote/service/GitHubService.kt
@@ -2,10 +2,15 @@ package br.com.prado.eduardo.luiz.githubrepositories.data.source.remote.service
 
 import br.com.prado.eduardo.luiz.githubrepositories.data.source.remote.dto.PageDTO
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface GitHubService {
 
-  @GET
-  suspend fun getRepositories(page: Int): PageDTO
+  @GET("search/repositories/")
+  suspend fun getRepositories(
+    @Query("q") language: String = "language:kotlin",
+    @Query("page") page: Int,
+    @Query("sort") sort: String = "stars"
+  ): PageDTO
 
 }
