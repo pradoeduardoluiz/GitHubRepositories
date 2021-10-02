a369d88db27b61c78b20d8843c41efea917f7a12
Luiz Eduardo do Prado <prado.eduardo.luiz@gmail.com>
2021-10-02 05:50:21 -0300

    feat: create empty recycler view

--- app/src/main/java/br/com/prado/eduardo/luiz/githubrepositories/GitHubRepositoriesApp.kt
+++ app/src/main/java/br/com/prado/eduardo/luiz/githubrepositories/GitHubRepositoriesApp.kt
@@ -1,6 +1,8 @@
 package br.com.prado.eduardo.luiz.githubrepositories
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class GitHubRepositoriesApp : Application() {
 }
--- app/src/main/java/br/com/prado/eduardo/luiz/githubrepositories/ui/repositories/RepositoriesFragment.kt
+++ app/src/main/java/br/com/prado/eduardo/luiz/githubrepositories/ui/repositories/RepositoriesFragment.kt
@@ -1,7 +1,32 @@
 package br.com.prado.eduardo.luiz.githubrepositories.ui.repositories
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import br.com.prado.eduardo.luiz.githubrepositories.R
+import br.com.prado.eduardo.luiz.githubrepositories.databinding.RepositoriesFragmentBinding
+import br.com.prado.eduardo.luiz.githubrepositories.extensions.viewBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class RepositoriesFragment : Fragment() {
+@AndroidEntryPoint
+class RepositoriesFragment : Fragment(R.layout.repositories_fragment) {
+
+  private val binding by viewBinding(RepositoriesFragmentBinding::bind)
+  private val viewModel: RepositoriesContract.ViewModel by viewModels<RepositoriesViewModel>()
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    bindInputs()
+    bindOutputs()
+  }
+
+  private fun bindInputs() {
+
+  }
+
+  private fun bindOutputs() {
+
+  }
 
 }
